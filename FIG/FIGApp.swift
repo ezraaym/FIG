6fa3ad2cd69060a926cf4c38b31f54cc2f5246b1
Ezra Haugabrooks <ezra@aymscores.com>
2024-10-24 13:35:10 -0400

    Initial commit of FIG project

---
+++ FIG/FIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIGApp.swift
+//  FIG
+//
+//  Created by E. Haugabrooks on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
