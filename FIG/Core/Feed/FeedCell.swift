64c7181199a1ac913bc794aac375a5a1de916b53
Ezra Haugabrooks <ezra@aymscores.com>
2024-10-25 07:00:32 -0400

    built a loginview, feed cell and view, and main table

---
+++ FIG/Core/Feed/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  FIG
+//
+//  Created by E. Haugabrooks on 10/24/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ FIG/Core/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  FIG
+//
+//  Created by E. Haugabrooks on 10/25/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
